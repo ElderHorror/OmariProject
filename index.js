fb561d39fe63ae561c84e20b44c9437e6dc5732c
ElderHorror <107815490+ElderHorror@users.noreply.github.com>
2022-11-19 04:13:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function setText() {
+  let paras = document.querySelector("span");
+  paras.innerText = "affordable";
+}
+
+function setText2() {
+  let paras = document.querySelector("span");
+  paras.innerText = "accessible";
+}
+
+setInterval(() => {
+  setText();
+}, 3000);
+
+setInterval(() => {
+  setText2();
+}, 6000);
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
